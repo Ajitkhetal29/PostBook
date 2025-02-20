43a4861a7555f38d8d20e2f8cc7b19e76dfbbcbe
iamajitkhetal <114768969+iamajitkhetal@users.noreply.github.com>
2025-02-20 23:42:07 +0530

    initial commit

---
+++ frontend/src/assets/images/assets.js
@@ -0,0 +1,10 @@
+import logo from "./logo.png"
+import profile from "./profile.png"
+import logout from "./logout.png"
+
+
+export const assets = {
+    logo,
+    profile,
+    logout
+}
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import { assets } from '../assets/images/assets'
+import "./navbar.css"
+
+const Navbar = () => {
+  return (
+    <div className=' navbar' >
+
+      <img src={assets.logo} alt="" width="40px" />
+
+      <ul>
+        <li><span><img src={assets.profile} alt=""  width="40px" /></span></li>
+        <li><span><img src={assets.logout} alt="" width="40px" /></span></li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default Navbar
